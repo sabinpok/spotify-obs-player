87a5528bc8036e37dcac060b8d0be116618f80f7
Sabin Pokhrel <118865601+sabinpok@users.noreply.github.com>
2024-05-03 01:32:45 -0400

    init for auto-id defined in program

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import services from "./services/info";
 import Card from "./components/card";
-import Form from "./components/form";
 import Description from "./components/description";
 import { useEffect, useState } from "react";
 import "./styles.css";
@@ -13,55 +12,35 @@ function App() {
     album_art_url: "https://placehold.co/400",
   });
 
-  const [userInfo, setUserInfo] = useState("");
-  const [newUserInfo, setNewUserInfo] = useState("");
-
-  const handleUserInfoChange = (event) => {
-    setNewUserInfo(event.target.value);
-  };
-
-  const handleUserInfoSubmit = (event) => {
-    event.preventDefault();
-    setUserInfo(newUserInfo);
-    setNewUserInfo("");
-  };
-
   useEffect(() => {
-    if (userInfo) {
-      const interval = setInterval(() => {
-        services
-          .getInfo(userInfo)
-          .then((result) => {
-            if (result.data.spotify !== null) {
-              setSongInfo({
-                song: result.data.spotify.song,
-                artist: result.data.spotify.artist,
-                album: result.data.spotify.album,
-                album_art_url: result.data.spotify.album_art_url,
-              });
-            }
-          })
-          .catch((error) => {
-            console.error(error);
+    const interval = setInterval(() => {
+      services
+        .getInfo()
+        .then((result) => {
+          if (result.data.spotify !== null) {
             setSongInfo({
-              song: "N/A",
-              artist: "N/A",
-              album: "N/A",
-              album_art_url: "https://placehold.co/400",
+              song: result.data.spotify.song,
+              artist: result.data.spotify.artist,
+              album: result.data.spotify.album,
+              album_art_url: result.data.spotify.album_art_url,
             });
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          setSongInfo({
+            song: "N/A",
+            artist: "N/A",
+            album: "N/A",
+            album_art_url: "https://placehold.co/400",
           });
-        return () => clearInterval(interval);
-      }, 5000);
-    }
-  }, [userInfo]);
+        });
+      return () => clearInterval(interval);
+    }, 5000);
+  }, []);
 
   return (
     <>
-      <Form
-        handleUserInfoSubmit={handleUserInfoSubmit}
-        newUserInfo={newUserInfo}
-        handleUserInfoChange={handleUserInfoChange}
-      />
       <Card songInfo={songInfo} />
       <Description />
     </>
--- src/services/info.js
+++ src/services/info.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const getInfo = (info) => {
-  const request = axios.get("https://api.lanyard.rest/v1/users/" + info);
+const getInfo = () => {
+  const request = axios.get(
+    "https://api.lanyard.rest/v1/users/603270966960848897" // Replace with your Discord ID, or just see what I'm listening to lmao
+  );
   return request.then((response) => response.data);
 };
 
